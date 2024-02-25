75fbc752772b5b275e4088859608397345be0ad0
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-25 19:15:10 +0530

    initialized python project

---
+++ main.py
@@ -0,0 +1,17 @@
+hello = '''Hello My name is {} and I am here to use python'''
+query = "kunal"
+if " is " in hello:
+    print("The word is present in the above string")
+else:
+    print("word is absent in above string ")
+
+hello = hello.format("kunal")
+print(hello)
+if query not in hello:
+    print(query + " word is absent in the above text")
+    print(hello.format(query))
+else:
+    print(hello.replace('kunal', 'null'))
+    print("after formatting the null")
+hi = bool('true')
+print(hi)
