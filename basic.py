70c5e1cfdf589663127e45a98fa9a06eccb5d985
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-03-25 03:44:11 +0530

    basic.py modified

---
+++ basic.py
@@ -0,0 +1,36 @@
+# this is the first shape that was made by printing the text
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/____|")
+
+character_name = "joker"
+
+character_age = "27"
+
+character_height = 172.345
+
+is_male = True
+
+is_tall = False
+
+print("there was a man once \nnamed \"" + character_name.upper() + "\"")
+
+my_variable = character_name.upper().isupper()
+
+index_of_k = character_name.index("k")
+
+print("index of k is " + str(index_of_k))
+
+print("first letter is : " + character_name[0])
+
+print("converting : " + character_name + " into new string : " + character_name.replace("ker", "b"))
+
+print("is the above line contains the word that is uppercase : " + str(my_variable))
+
+print("and he was  " + character_age + " years old")
+
+character_name = "kunal"
+
+print(character_name + " went to office yesterday at 1 am.")
